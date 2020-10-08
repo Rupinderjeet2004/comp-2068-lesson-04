3b98f2e9e9aa387c5ef7118d65d858af75a148f9
Rupinderjeet Singh <rupinderjeetsingh199@gmail.com>
2020-10-08 18:57:44 -0400

    Completed Tic Tac Toe

--- src/components/TicTacToe/index.jsx
+++ src/components/TicTacToe/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Header from '../shared/Header';
-
+import Board from './Board';
 const TicTacToe = () => {
   return (
     <>
       <Header title="Let's Do This Thang"/>
+      <Board/>
     </>
   );
 }
